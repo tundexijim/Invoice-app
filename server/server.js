e5454b1f719e6d7813a4d7a263ed0236908f42c8
tundexijim <freetunde2008@yahoo.com>
2023-07-25 01:36:15 +0300

    apollo

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ async function startServer() {
 
   await server.start();
 
-  server.applyMiddleware({path:"graphql", app });
+  server.applyMiddleware({ app });
 
   app.listen({ port: 4000 }, () => {
     console.log(`Server running at http://localhost:4000${server.graphqlPath}`);
